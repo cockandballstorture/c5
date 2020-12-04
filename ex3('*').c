af9279352f6483516f10d6c8fc4c0028099e8609
cockandballstorture <72618474+cockandballstorture@users.noreply.github.com>
2020-12-04 22:29:35 +0300

    Create ex3('*').c

---
+++ ex3('*').c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+void main()
+{   int n,m,p,q,i,e;
+    char a[35];
+    char r[35];
+    scanf("%15s", a);
+    n=strlen (a);
+    char y[50]="****************";
+    m=(16-n);
+    if(n<16){
+       strncat(a, y, m);
+    }
+    printf("%s\n",a);
+    for (i=8;i<16;i++){
+       a[i]=a[15-i];
+    }
+    printf("%s",a);
+}
