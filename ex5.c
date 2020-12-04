0286f22379219020b2ffe93efa7b5644573b0eca
cockandballstorture <72618474+cockandballstorture@users.noreply.github.com>
2020-12-04 17:56:59 +0300

    Create ex5.c

---
+++ ex5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+void rate();
+void main()
+{  
+    int e,i;
+    float q,k;
+    float h[151];
+    int a=1;
+    e=0;
+    printf("rate? y1/n2");
+    scanf("%c",a);
+    while (a==1){
+       printf("?/5");
+       scanf("%f",k);
+       q=(q/2)+k;
+       e++;
+       h[e]=k;
+       if (e==151){
+           q=q-(h[0]/(pow(2,150)));
+           for (i=0;i<151;i++){
+              h[i]=h[i+1];
+              e=150;
+           }
+       }
+       printf("rate? y/n");
+       scanf("%c",a);
+    }
+}
