7751afea3245657381ddbbddc3c0454a1f4884e5
cockandballstorture <72618474+cockandballstorture@users.noreply.github.com>
2020-12-04 16:18:44 +0300

    Update ex0(1).c

--- ex0(1).c
+++ ex0(1).c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-void main()
-{   
+int main()
+{ 
     int s=1;
-    const int *p = &s;
-    printf("%d\n",*p);
+    int *p;
+    int *q;
+    p=&s;
+    q=p;
+    printf("%d,%d\n",*p,*q);
     printf("%d\n",*p+1);
-    printf("%d,%d",*(p++),*(p--));
+    *p--;
+    *q++;
+    printf("%d,%d",*p,*q);
 }
